e37cc40da331f510193f4eb5c34752e7b0ccb1a4
euwilliamsantos <euwilliam.77@gmail.com>
2024-04-21 03:17:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Autor: william alves dos santos,Vinicius ferreira viana
+//Data 21/04/2024
+//Trabalho Final da Unidade - Avaliação Prática de LP
+
+
+
+var Nome = prompt('Digite seu nome completo')
+var jogo = prompt('Agora me fala quem descobrio o Brasil?')
+
+
+alert('Bem vindo a Netflix:'+ Nome +  'Resultado: ' + jogo )
