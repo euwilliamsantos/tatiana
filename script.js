d66eed8081ffec3f5ea6f6aef1265639b180f673
euwilliamsantos <145413257+euwilliamsantos@users.noreply.github.com>
2024-04-22 23:50:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-//Autor: william alves dos santos,Vinicius ferreira viana
-//Data 21/04/2024
-//Trabalho Final da Unidade - Avaliação Prática de LP
 
+//Autor: William Alves dos Santos, Vinicius Ferreira Viana
+//Data: 21/04/2024
+//Trabalho Final da Unidade - Avaliação Prática de Linguagem de Programação
+
+//Descrição do Projeto:
+//Este projeto consiste na criação de uma tela de login inspirada na série "Stranger Things". A tela foi desenvolvida sem a integração de um banco de dados, focando exclusivamente na interface. A interação do usuário é realizada por meio do JavaScript, que inclui um prompt como entrada de dados e um alert como saída de dados e um favicon,
+// sem muita complexidade.
 
 
 var Nome = prompt('Digite seu nome completo')
-var jogo = prompt('Qual a função da Netflix?')
+var Idade = prompt('Escreva sua idade')
 
 
-alert('Bem vindo a Netflix:'+ Nome +  'Resultado: ' + jogo )
+alert('Bem vindo a Netflix:'+ Nome +  'Você tem: '  +  Idade  + 'Anos')
